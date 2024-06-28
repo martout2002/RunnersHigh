b9212e6d1f52a053ac3b1a01e2e18a6189cfafc9
Shaun <Hundin231@gmail.com>
2024-06-28 14:32:20 +0800

    added friend page

---
+++ lib/social/friend_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FriendPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Friend Page'),
+      ),
+      body: Center(
+        child: Text('Friend Page'),
+      ),
+    );
+  }
+}
